f4abdf937d5e5db27503343f3a41fd88f07f4b18
Sonar <Sonar@DANGAO.hsd1.or.comcast.net>
2015-02-01 20:27:43 -0800

    full screen by default

--- src/demo/DemoProject.java
+++ src/demo/DemoProject.java
@@ -14,7 +14,8 @@ public class DemoProject {
     //running this file should do the trick
     public static void main(String[] args) {
         GameFrame g=new GameFrame(new DemoLoop(320,240),"Demo Project",1000);
-        g.setSize(640, 480);
+        //g.setSize(640, 480);
+        g.setFullScreen(true);
         g.Run();
     }
 }
